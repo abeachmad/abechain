856c0bb8751a36b67de08e476f520ccdb8d59e2f
abeachmad <96498781+abeachmad@users.noreply.github.com>
2022-10-02 19:53:54 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,13 +7,13 @@
         <h1
           class="text-primary display-4 font-weight-bolder d-none d-md-block"
         >
-          MDLOG Dashboard<small class="flow-left">Beta</small>
+          AbeClog Explorer<small class="flow-left">Beta</small>
         </h1>
       </div>
     </b-link>
 
     <p class="mb-1">
-      MDLOG Dashboard is not just an explorer but also a wallet and more ... 🛠
+      AbeClog Explorer is not just an explorer but also a wallet and more ... 🛠
     </p>
     <h2 class="mb-3">
       Cosmos Ecosystem Blockchains 🚀
